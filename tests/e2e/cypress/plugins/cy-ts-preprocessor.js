4f8928dbe3d0acc787e1024f38ba762870025b4f
Edgar Mejia <edgar.mejia@rootstrap.com>
2022-01-24 11:38:33 -0500

    add coverall dependency remove root path webpack publicpath change cypress loader ts-loader to babel-loader

--- tests/e2e/cypress/plugins/cy-ts-preprocessor.js
+++ tests/e2e/cypress/plugins/cy-ts-preprocessor.js
@@ -10,7 +10,7 @@ module.exports = wp({
         {
           test: /\.ts$/,
           exclude: /node_modules/,
-          loader: 'ts-loader',
+          loader: 'babel-loader',
         },
       ],
     },
